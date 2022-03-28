1fb934cf4a9b167b1d039a5f3cb5885394ae00f3
mcstewart76 <mcstewart76@gmail.com>
2022-03-27 20:33:18 -0400

    Added about page handlebar viewer and fixed some href stuff

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,4 +5,17 @@ router.get('/', (req, res) => {
 res.render('homepage')
 });
 
+router.get('/about', (req,res) => {
+    res.render('about')
+});
+router.get('/hobbies', (req,res) => {
+    res.render('hobbies')
+});
+router.get('/resume', (req,res) => {
+    res.render('resume')
+});
+router.get('/works', (req,res) => {
+    res.render('works')
+});
+
 module.exports = router;
